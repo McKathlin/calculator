7bb0f30bf49d1ada4694d3f320684d952b49e0ce
McKathlin <mckathlin@gmail.com>
2024-11-01 12:46:31 +0000

    Make Clear and Evaluate buttons respond to click
    This revision makes the Clear button work as intended.
    The Evaluate '=' button does not calculate yet; it always causes
    ERROR to display.

--- calculator.js
+++ calculator.js
@@ -1,30 +1,51 @@
 //=============================================================================
-// Globals
+// Calculator
+// by McKathlin
 //=============================================================================
 
 const Calculator = {};
-
-Calculator.screenNode = document.querySelector("input#screen");
 
 //=============================================================================
 // UI Setup
 //=============================================================================
 
+Calculator.screenInput = document.querySelector("input#screen");
+console.log(Calculator.screenNode);
 
+document.querySelector("button#evaluate").addEventListener("click", (e) => {
+    console.log("Evaluate");
+    const expression = Calculator.screenInput.value;
+    const result = Calculator.evaluateExpression(expression);
+    const resultText = Number.isNaN(result) ? "ERROR" : result.toString();
+    Calculator.screenInput.value = resultText;
+});
+
+document.querySelector("button#clear").addEventListener("click", (e) => {
+    console.log("Clear");
+    Calculator.screenInput.value = "";
+});
+
+// TODO: Implement backspace button
+
+// TODO: Implement number buttons
+
+// TODO: Implement decimal point button
+
+// TODO: Implement operator buttons
 
 //=============================================================================
 // Calculator Logic
 //=============================================================================
-// Input processing
+// Public Controls
 //-----------------------------------------------------------------------------
 
+Calculator.evaluateExpression = function(expression) {
+    // TODO: Implement
+    return Number.NaN;
+};
 
 //-----------------------------------------------------------------------------
-// Expression Evaluation
+// Private Helper Methods
 //-----------------------------------------------------------------------------
 
-
-//-----------------------------------------------------------------------------
-// Arithmetic
-//-----------------------------------------------------------------------------
 
