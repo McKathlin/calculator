1e9089c5f93c856c955a1b77bf20b26277de1460
McKathlin <mckathlin@gmail.com>
2024-11-04 15:10:00 +0000

    Implement decimal point and account for errors

--- calculator.js
+++ calculator.js
@@ -8,8 +8,9 @@ Calculator.buttonType = { none: 0, digit: 1, operator: 2, unary: 3 };
 Calculator.EMPTY_TEXT = "0";
 Calculator.ERROR_TEXT = "ERROR";
 
+Calculator.errorState = false;
 Calculator.currentText = "";
-Calculator.previousOperand = null;
+Calculator.previousNumber = null;
 Calculator.operateFunction = null;
 Calculator.lastButtonType = Calculator.buttonType.none;
 
@@ -61,12 +62,13 @@ document.addEventListener("DOMContentLoaded", (e) => {
 //=============================================================================
 
 Calculator.updateUI = function() {
-    Calculator.resultNode.innerText = Calculator.currentText;
+    if (Calculator.errorState) {
+        Calculator.resultNode.innerText = Calculator.ERROR_TEXT;
+    } else {
+        Calculator.resultNode.innerText = Calculator.currentText;
+    }
+    
     // TODO: Enable/disable buttons as needed
-};
-
-Calculator.getCurrentNumber = function() {
-    return Number.parseFloat(Calculator.currentText);
 };
 
 //=============================================================================
@@ -74,7 +76,14 @@ Calculator.getCurrentNumber = function() {
 //=============================================================================
 
 Calculator.backspace = function() {
-    Calculator.currentText = Calculator.currentText.slice(0, -1);
+    if (Calculator.errorState) {
+        // Don't change error state.
+    } else if (Calculator.currentText == Calculator.EMPTY_TEXT) {
+        // Already empty. Do nothing.
+    } else {
+        // Remove the last character.
+        Calculator.currentText = Calculator.currentText.slice(0, -1);
+    }
     Calculator.updateUI();
 };
 
@@ -82,11 +91,20 @@ Calculator.clear = function() {
     Calculator.previousOperand = null;
     Calculator.operateFunction = null;
     Calculator.currentText = Calculator.EMPTY_TEXT;
+    Calculator.errorState = false;
     Calculator.updateUI();
 };
 
 Calculator.appendDigit = function(digit) {
     // TODO
+};
+
+Calculator.appendDecimalPoint = function() {
+    if (Calculator.currentText.includes(".")) {
+        Calculator.errorState = true;
+    } else {
+        Calculator.currentText += ".";
+    }
 };
 
 Calculator.setBinaryOperator = function(operatorName) {
@@ -123,4 +141,12 @@ Calculator.squareRoot = function(num) {
 
 Calculator.square = function(num) {
     return num * num;
+};
+
+//-----------------------------------------------------------------------------
+// Helper methods
+//-----------------------------------------------------------------------------
+
+Calculator._getCurrentNumber = function() {
+    return Number.parseFloat(Calculator.currentText);
 };
